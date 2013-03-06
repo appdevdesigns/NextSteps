be7739e1d00a3aef015fdd1e4e9bc0dc94c344cf
Caleb Cox <calebcox314@gmail.com>
2013-03-05 20:44:50 -0600

    Specify sync server URL in preferences pane

--- Resources/defaults.js
+++ Resources/defaults.js
@@ -1,10 +1,5 @@
 var defaults = {
     // Place other application specific defaults here
-    Model: {
-        defaultConnectionType: 'local'
-    },
-    localStorageEnabled: true,
-    serverStorageEnabled: false
 };
 
 module.exports = defaults;
