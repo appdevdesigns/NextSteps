cb547aff2e5456db16ac533b09fb3f9df707aebc
Caleb Cox <calebcox314@gmail.com>
2013-07-18 21:34:05 -0500

    Add a "Tools" window with preliminary support for backing up and restoring the database

---
+++ Resources/ui/AppToolsWindow.js
@@ -0,0 +1,81 @@
+var AD = require('AppDev');
+var $ = require('jquery');
+
+module.exports = $.Window('AppDev.UI.AppToolsWindow', {
+    dependencies: ['GoogleDriveChooseFileWindow']
+}, {
+    init: function(options) {
+        // Initialize the base $.Window object
+        this._super({
+            title: 'toolsTitle',
+            createParams: {
+                layout: 'vertical'
+            }
+        });
+    },
+
+    // Create the child views
+    create: function() {
+        var tab = this.tab;
+        var DataStore = require('appdev/db/DataStoreSQLite');
+
+        this.add(Ti.UI.createLabel({
+            left: AD.UI.padding,
+            top: AD.UI.padding,
+            width: AD.UI.SIZE,
+            height: AD.UI.SIZE,
+            textid: 'backupRestoreInfoText',
+            font: AD.UI.Fonts.mediumSmall
+        }));
+        var backupButton = this.add('backup', Ti.UI.createButton({
+            top: AD.UI.padding,
+            center: {
+                x: AD.UI.screenWidth / 2
+            },
+            width: 120,
+            height: AD.UI.buttonHeight,
+            titleid: 'backupDatabase'
+        }));
+        backupButton.addEventListener('click', function() {
+            // Calculate the backup title
+            var today = new Date();
+            var backupTitle = $.formatString('backupTitleFormat',
+                ('0' + (today.getMonth() + 1)).slice(-2),
+                ('0' + today.getDate()).slice(-2),
+                today.getFullYear());
+            DataStore.export(AD.Defaults.dbName).done(function(dump) {
+                AD.Comm.GoogleDriveFileAPI.write({
+                    content: JSON.stringify(dump),
+                    metadata: {
+                        title: backupTitle,
+                        mimeType: 'application/json',
+                        parents: ['root']
+                    }
+                });
+            });
+        });
+
+        var restoreButton = this.add('restore', Ti.UI.createButton({
+            top: AD.UI.padding,
+            center: {
+                x: AD.UI.screenWidth / 2
+            },
+            width: 120,
+            height: AD.UI.buttonHeight,
+            titleid: 'restoreDatabase'
+        }));
+        restoreButton.addEventListener('click', function() {
+            // Let the user choose the file to restore from
+            var $winChooseFile = new AD.UI.GoogleDriveChooseFileWindow({
+                tab: tab,
+                type: 'file',
+                folder: null
+            });
+            $winChooseFile.getDeferred().done(function(fileId) {
+                AD.Comm.GoogleDriveFileAPI.read(fileId, function(dump) {
+                    DataStore.import(AD.Defaults.dbName, JSON.parse(dump));
+                });
+            });
+        });
+    }
+});
