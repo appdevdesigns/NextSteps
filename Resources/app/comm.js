f34f95e1a8c9feb8fe164991de9143cd038e3dd0
Caleb Cox <calebcox314@gmail.com>
2014-02-26 12:20:08 +0800

    Application-specific configuration and communication methods

---
+++ Resources/app/comm.js
@@ -0,0 +1,24 @@
+var AD = require('appdev');
+var $ = require('jquery');
+
+module.exports = $.extend(AD.Comm, {
+    pingServer: function(server) {
+        return AD.Comm.HTTP.get({
+            url: 'http://'+server+'/nsserver/ping'
+        });
+    },
+    
+    // Make an authentication request to the specified server using the provided username and password credentials
+    // Return a deferred object that will resolve to true if the credentials are valid, false if they are not
+    validateCredentials: function(server, username, password) {
+        return AD.Comm.HTTP.post({
+            url: 'http://'+server+'/nsserver/auth',
+            params: {
+                username: username,
+                password: password
+            }
+        }).pipe(function(response) {
+            return response.status === 'success';
+        });
+    }
+});
---
+++ Resources/app/config.js
@@ -0,0 +1,8 @@
+var config = module.exports = {
+    getServer: function() {
+        return Ti.App.Properties.getString('server_url_preference');
+    },
+    hasServer: function() {
+        return config.getServer() !== '';
+    }
+};
