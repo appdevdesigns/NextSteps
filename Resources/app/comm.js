912f8627cecec9271db135c70d342b2407b5c651
Caleb Cox <calebcox314@gmail.com>
2014-03-27 20:06:22 -0500

    Add AD.Comm.appdevRequest for requests to AppDev servers

--- Resources/app/comm.js
+++ Resources/app/comm.js
@@ -11,7 +11,8 @@ module.exports = $.extend(AD.Comm, {
     // Make an authentication request to the specified server using the provided username and password credentials
     // Return a deferred object that will resolve to true if the credentials are valid, false if they are not
     validateCredentials: function(server, username, password) {
-        return AD.Comm.HTTP.post({
+        return AD.Comm.appdevRequest({
+            method: 'POST',
             url: 'http://'+server+'/nsserver/auth',
             params: {
                 username: username,
@@ -25,14 +26,15 @@ module.exports = $.extend(AD.Comm, {
             console.log("response (fail) = ");
             console.log(response);
             return false;
-        });
+        }).promise();
     },
     
     // Send the provided transactions to the server and return a deferred
     // that will resolve to the transaction log received from the server
     syncWithServer: function(server, transactions, username, password) {
         var syncDfd = $.Deferred();
-        AD.Comm.HTTP.post({
+        AD.Comm.appdevRequest({
+            method: 'POST',
             url: 'http://'+server+'/nsserver/sync',
             params: {
                 username: username,
@@ -46,13 +48,24 @@ module.exports = $.extend(AD.Comm, {
             console.log(response);
             AD.PropertyStore.set('lastSyncServer', server);
             AD.PropertyStore.set('lastSyncTimestamp', response.data.lastSyncTimestamp);
-            if (response.status === 'success') {
-                syncDfd.resolve(response.data.transactionLog);
-            }
-            else {
-                syncDfd.reject(response);
-            }
+            syncDfd.resolve(response.data.transactionLog);
         }).fail(syncDfd.reject);
         return syncDfd.promise();
+    },
+    
+    // Make a request to an AppDev resource
+    appdevRequest: function(options) {
+        var requestDfd = $.Deferred();
+        AD.Comm.HTTP.request(options).done(function(response) {
+            if (response.status === 'success') {
+                // The request succeeded
+                requestDfd.resolve(response);
+            }
+            else {
+                // The request failed
+                requestDfd.reject(response);
+            }
+        }).fail(requestDfd.reject);
+        return requestDfd.promise();
     }
 });
