bddb3e0fe5d49ced4b386af4319ecd9e4048cd30
Caleb Cox <calebcox314@gmail.com>
2015-12-12 11:08:24 -0600

    Inform users of how to migrate their data to the new app

--- Resources/app.js
+++ Resources/app.js
@@ -6,11 +6,18 @@
 
 // Initialize the AppDev framework
 var AD = require('AppDev');
+var $ = require('jquery');
 
 AD.init({
     models: ['Viewer', 'Contact', 'Group', 'Campus', 'Year', 'Tag', 'ContactTag', 'Step', 'ContactStep'],
     windows: ['AppContactsWindow', 'AppGroupsWindow', 'AppStatsWindow', 'AppToolsWindow', 'AppInfoWindow']
 }).done(function() {
+    // Show instructions for how to transfer data from the old NextSteps app just one time
+    if (!Ti.App.Properties.getBool('dataTransferInstructionsShown', false)) {
+        alert($.formatString('dataTransferInstructions', AD.Defaults.feedbackAddress));
+        Ti.App.Properties.setBool('dataTransferInstructionsShown', true);
+    }
+    
     var controller = require('app/controller');
     controller.start();
 });
